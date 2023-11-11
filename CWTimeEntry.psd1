e326b18679db07cd7f4e3739f338502dd7198ca4
Joseph Robertson <josephprobertson20@proton.me>
2023-11-11 11:52:57 -0500

    Layed out functions and cleaned everything

--- CWTimeEntry.psd1
+++ CWTimeEntry.psd1
@@ -12,7 +12,7 @@
 RootModule = 'CWTimeEntry.psm1'
 
 # Version number of this module.
-ModuleVersion = '0.0.1'
+ModuleVersion = '0.0.2'
 
 # Supported PSEditions
 # CompatiblePSEditions = @()
@@ -69,10 +69,10 @@ PowerShellVersion = '5.1'
 # NestedModules = @()
 
 # Functions to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no functions to export.
-FunctionsToExport = @()
+FunctionsToExport = @('Get-CWTimeEntry','Remove-CWTimeEntry')
 
 # Cmdlets to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no cmdlets to export.
-CmdletsToExport = @()
+# CmdletsToExport = @('Get-CWTimeEntry','Remove-CWTimeEntry')
 
 # Variables to export from this module
 VariablesToExport = '*'
--- CWTimeEntry.psm1
+++ CWTimeEntry.psm1
@@ -1,4 +1,101 @@
-function HelloWorld
+$script:config   = Get-Content $PSScriptRoot\config.json | ConvertFrom-Json
+$script:template = Get-Content $PSScriptRoot\template.txt -Raw
+
+
+function New-CWTime
 {
-    Write-Information "Hello world!"
+    [CmdletBinding()]
+    param()
+
+    Write-Information "Remove!"
+}
+
+function Remove-CWTime
+{
+    [CmdletBinding()]
+    param()
+
+    Write-Information "Remove!"
+}
+
+function Get-CWTime
+{
+    [CmdletBinding()]
+    param(
+        [datetime]$Date = (Get-Date)
+    )
+
+    Write-Information "Get!"
+}
+
+function Clear-CWTime
+{
+    [CmdletBinding()]
+    param(
+        [datetime]$Date = (Get-Date)
+    )
+
+    Write-Information "Remove!"
+}
+
+function Send-CWTimeNote
+{
+    [CmdletBinding()]
+    param(
+        [datetime]$Date = (Get-Date)
+    )
+
+    Write-Host $Date
+
+    Write-Information "Get!"
+}
+
+function Show-CWTimeNote
+{
+    [CmdletBinding()]
+    param(
+        [datetime]$Date = (Get-Date)
+    )
+
+    $datestring = $date.ToString("yyyy-MM-dd")
+    $TimeNotePath = "$PSScriptRoot\TimeNotes\$datestring.txt"
+
+    if(-not (Test-Path $TimeNotePath))
+    {
+        $script:template > $TimeNotePath
+    }
+
+    Start-Process -FilePath $script:config.TextEditor -ArgumentList $TimeNotePath
+}
+
+function Show-CWConfig
+{
+    [CmdletBinding()]
+    param()
+
+    Start-Process -FilePath $script:config.TextEditor -ArgumentList "$PSScriptRoot\config.json"
+}
+
+function Show-CWLog
+{
+    [CmdletBinding()]
+    param()
+
+    Start-Process -FilePath $script:config.TextEditor -ArgumentList "$PSScriptRoot\log.txt"
+}
+
+function Show-CWTickets
+{
+    [cmdletbinding()]
+    param()
+
+    Start-Process -FilePath $script:config.TextEditor -ArgumentList "$PSScriptRoot\tickets.txt"
+}
+
+function Show-CWTemplate
+{
+    [cmdletbinding()]
+    param()
+
+    Start-Process -FilePath $script:config.TextEditor -ArgumentList "$PSScriptRoot\template.txt"
 }
